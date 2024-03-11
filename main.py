a81593aca86c2f53d3ff5d71a0486274dcf77a62
Jorick <jorick.python@gmail.com>
2024-03-11 18:34:48 +0800

    remove dropbox integration and introduce manual image retriever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask import Flask, abort, render_template, redirect, url_for, flash, request, session
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
@@ -10,20 +10,20 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from sqlalchemy import desc
-import dropbox
 import setuptools
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
 
 
+# flask setup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 app.config['UPLOAD_FOLDER'] = os.environ.get('UPLOAD_FOLDER')
-dbx = dropbox.Dropbox(os.environ.get("DROPBOX_ACCESS_TOKEN"))
+
+# html setup
 ckeditor = CKEditor(app)
 Bootstrap5(app)
-
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -33,6 +33,7 @@ login_manager.init_app(app)
 def load_user(user_id):
     return db.get_or_404(User, user_id)
 
+
 def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -40,35 +41,19 @@ def admin_only(f):
             error = 'Sorry, this page is Admin only, please log in as Admin to access'
             return redirect(url_for('login', error=error))
         return f(*args, **kwargs)
+
     return decorated_function
+
+def get_image_files(post_id):
+    folder_path = os.environ.get("FOLDER_PATH") + str(post_id)
+    image_files = os.listdir(folder_path)
+    return image_files
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
 
-def get_dropbox_image_files(folder_path):
-    image_links = []
-    try:
-        # List files in the specified folder
-        folder_list = dbx.files_list_folder(folder_path)
-        for entry in folder_list.entries:
-            if isinstance(entry, dropbox.files.FileMetadata) and entry.name.lower().endswith(
-                    ('.png', '.jpg', '.jpeg', '.gif')):
-                    list_links = dbx.sharing_list_shared_links(entry.path_lower).links
-                    if list_links:
-                        existing_link = list_links[0].url
-                        existing_link = existing_link.replace('dl=0', 'dl=1')  # Convert to direct download link
-                        image_links.append(existing_link)
-                    else:
-                        link = dbx.sharing_create_shared_link_with_settings(entry.path_lower)
-                        direct_link = link.url.replace('dl=0', 'dl=1')  # Convert to direct download link
-                        image_links.append(direct_link)
-    except dropbox.exceptions.ApiError as e:
-        print(f"Error listing folder: {e}")
-    return image_links
-
-
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -83,7 +68,6 @@ class BlogPost(db.Model):
     body = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
-
 
 
 class User(UserMixin, db.Model):
@@ -102,23 +86,24 @@ class Comment(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
 
+
 with app.app_context():
     db.create_all()
 
 
-@app.route('/register', methods=["GET","POST"])
+@app.route('/register', methods=["GET", "POST"])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        error=None
+        error = None
         email = form.data['email']
         password = generate_password_hash(form.data['password'], method='pbkdf2:sha256', salt_length=8)
         # checking if email is already in database and referring to login page
         result = db.session.execute(db.select(User).where(User.email == email))
         existing_user = result.scalar()
         if existing_user:
-            error = "Sorry, this email already exists, try logging in instead:"
-            return redirect(url_for('login',error=error))
+            error = "Sorry, this email already exists, try logging inx instead:"
+            return redirect(url_for('login', error=error))
 
         # Save the uploaded image to the server
         img_file = form.img_url.data
@@ -133,7 +118,7 @@ def register():
             name=form.data['name'],
             email=email,
             password=password,
-            img_url = img_url
+            img_url=img_url
         )
         db.session.add(new_user)
         db.session.commit()
@@ -142,8 +127,7 @@ def register():
     return render_template("register.html", form=form)
 
 
-
-@app.route('/login', methods=["GET","POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     error = request.args.get('error')
     form = LoginForm()
@@ -155,7 +139,7 @@ def login():
         result = db.session.execute(db.select(User).where(User.email == email))
         user = result.scalar()
         if not user:
-            error= "E-mail not found. If you don't have an account, register instead."
+            error = "E-mail not found. If you don't have an account, register instead."
         elif not check_password_hash(user.password, password):
             error = 'Wrong password, please try again'
         else:
@@ -183,7 +167,6 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 
-
 @app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
@@ -192,26 +175,22 @@ def show_post(post_id):
     posts = post_list.scalars().all()
     if form.validate_on_submit():
         new_comment = Comment(
-        comment = form.data['comment'],
-        post_id=requested_post.id,
-        author = current_user.name,
-        img_url = current_user.img_url,
-        date = date.today().strftime("%B %d, %Y")
+            comment=form.data['comment'],
+            post_id=requested_post.id,
+            author=current_user.name,
+            img_url=current_user.img_url,
+            date=date.today().strftime("%B %d, %Y")
         )
         db.session.add(new_comment)
         db.session.commit()
         result = db.session.execute(db.select(Comment).order_by(desc(Comment.id)))
         comments = result.scalars().all()
         return redirect(url_for("show_post", post_id=post_id, all_comments=comments, all_posts=posts))
-
     result = db.session.execute(db.select(Comment).order_by(desc(Comment.id)))
     comments = result.scalars().all()
-    dropbox_folder_path = os.environ.get("DROPBOX_FOLDER_PATH")+str(post_id)
-    image_files = get_dropbox_image_files(dropbox_folder_path)
-
+    image_files = get_image_files(post_id)
     return render_template("post.html", post=requested_post, form=form,
-                           all_comments=comments, all_posts=posts, image_files=image_files)
-
+                           all_comments=comments, all_posts=posts, image_files=image_files, post_id=str(post_id))
 
 
 @app.route("/new-post", methods=["GET", "POST"])
@@ -231,7 +210,6 @@ def add_new_post():
         db.session.commit()
         return redirect(url_for("get_all_posts"))
     return render_template("make-post.html", form=form)
-
 
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
@@ -256,7 +234,6 @@ def edit_post(post_id):
     return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
-
 @app.route("/delete/<int:post_id>")
 @admin_only
 def delete_post(post_id):
@@ -264,6 +241,7 @@ def delete_post(post_id):
     db.session.delete(post_to_delete)
     db.session.commit()
     return redirect(url_for('get_all_posts'))
+
 
 @app.route("/delete/<int:comment_id>/<int:post_id>")
 @admin_only
@@ -273,10 +251,10 @@ def delete_comment(comment_id, post_id):
     db.session.commit()
     return redirect(url_for("show_post", post_id=post_id))
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
-
 
 
 if __name__ == "__main__":
